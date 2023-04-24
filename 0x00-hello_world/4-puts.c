9b58fbfede3cfbbcca5990900f9db65a874b0a43
glendamotsi5 <glendamotsi5@gmail.com>
2023-04-24 06:25:18 -0700

    5. Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: always 0 (success)
+ * main - a program that prints a line with puts function
+ * return : always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - a program that prints a line with printf function
+  * Return : Always 0 (success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
