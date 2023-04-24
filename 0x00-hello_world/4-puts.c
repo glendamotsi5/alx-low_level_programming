2a483c5141ed6668fee5283d1bbb90a29931eb0c
glendamotsi5 <glendamotsi5@gmail.com>
2023-04-24 01:39:30 -0700

    u+x

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * return: always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
